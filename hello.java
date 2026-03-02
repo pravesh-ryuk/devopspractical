9418b1efd501effe6b160842ebe74168ae6b6bdd
pravesh-ryuk <praveshgarpalliwar@gmail.com>
2026-03-02 14:22:27 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
 public static void main(String[] args){
         System.out.println("Hello world");
-        System.out.println("Addition: "+(2+3));
+        System.out.println("Addition: "+(20+3));
          System.out.println("Hello john");
 }
 }
